f3ad88e129326609f7f4cbbccdab25c5d2ddb65a
Arhambackup <snehathedramaqueen@gmail.com>
2026-02-24 21:49:42 +0600

    main element added

---
+++ script.js
@@ -0,0 +1,6 @@
+// main elements
+const jobListContainer = document.getElementById('job-list-container');
+const totalCountEl = document.getElementById('total-count');
+const interviewCountEl = document.getElementById('interview-count');
+const rejectedCountEl = document.getElementById('rejected-count');
+const jobStatLabel = document.getElementById('job-stat-label');
