ec0bdafb8670662067ad987671a02fe07612cf2d
Arhambackup <snehathedramaqueen@gmail.com>
2026-02-24 21:53:50 +0600

    top and sidbae

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const totalCountEl = document.getElementById('total-count');
 const interviewCountEl = document.getElementById('interview-count');
 const rejectedCountEl = document.getElementById('rejected-count');
 const jobStatLabel = document.getElementById('job-stat-label');
+
+/**
+ * all stats at the top and the sidebar label
+ */
+function updateAllStats() {
+    const allCards = document.querySelectorAll('.job-card');
+    const iCount = document.querySelectorAll('.job-card[data-status="interview"]').length;
+    const rCount = document.querySelectorAll('.job-card[data-status="rejected"]').length;
+
+    totalCountEl.textContent = allCards.length;
+    interviewCountEl.textContent = iCount;
+    rejectedCountEl.textContent = rCount;
+    jobStatLabel.textContent = `${allCards.length} jobs`;
+}
